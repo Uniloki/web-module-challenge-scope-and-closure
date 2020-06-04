64298db052f2314ec8cebbd54a6ed515ef4c394a
unknown <uniloki009@gmail.com>
2020-06-03 23:25:38 -0400

    mvp done

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ * coutner one stores everything locally and counter 2 stores count globally
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ * coutner1 is using closure
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ *counter 1 is going to be mostly preferable because count can be reused, counter 2 would be if you want to use that numer in mltiple functions.
 */
 
 // counter1 code
@@ -51,46 +51,42 @@ function counter2() {
   return count++;
 }
 
-
+console.log ("hello");
 /* Task 2: inning() 
-
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+return Math.floor(Math.random() * 3);
 }
 
 /* Task 3: finalScore()
-
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
-
 For example, 
-
 finalScore(inning, 9) might return: 
 {
   "Home": 11,
   "Away": 5,
 }
-
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inning,numOfIn){
+  let home = 0;
+  let away = 0;
+  for (i = 0; i < numOfIn; i++){
+    home = home + inning();
+    away = away + inning();
+  }
+  return {
+    "Home": home,
+    "Away": away
+  }; 
 }
 
 /* Task 4: 
-
 Create a function called `scoreboard` that accepts the following parameters: 
-
 (1) Callback function `inning` that you wrote above
 (2) A number of innings
-
 and returns the score at each pont in the game, like so:
-
 1st inning: 0 - 2
 2nd inning: 1 - 3
 3rd inning: 1 - 3
@@ -100,11 +96,22 @@ and returns the score at each pont in the game, like so:
 7th inning: 4 - 6
 8th inning: 5 - 8
 9th inning: 6 - 10
-
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(inning,numOfIn) {
+  let home = 0;
+  let away = 0;
+  let homeA = [];
+  let awayA = [];
+  for(i = 0; i < numOfIn; i++){
+    home = home + inning();
+    homeA.push(home);
+    away = away + inning();
+    awayA.push(away);
+    console.log(
+      'Inning', i + 1 ,'Home:' ,homeA[i],
+      'Away:' ,awayA[i]
+    )
+  }
+  console.log (homeA,awayA);
 }
-
-
